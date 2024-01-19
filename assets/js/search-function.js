f02f966767962dd63e261080cf1366895f25a75b
nobi102723 <156570255+nobi102723@users.noreply.github.com>
2024-01-19 17:40:56 +0800

    revamped search function

--- assets/js/search-function.js
+++ assets/js/search-function.js
@@ -2,16 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('searchForm');
     const input = document.getElementById('inputModalSearch');
     const modal = new bootstrap.Modal(document.getElementById('templatemo_search')); // Initialize the modal
-    
-    form.addEventListener('submit', function (event) {
 
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
         const searchTerm = input.value.toLowerCase();
-                
-        // Redirect to the search results page with the search term as a query parameter
-        window.location.href = `search.html?q=${encodeURIComponent(searchTerm)}`;
-                
+
         // Hide the modal after handling the search (you can remove this line if you want to keep the modal open)
+        window.location.href = `search.html?q=${encodeURIComponent(searchTerm)}`;
         modal.hide();
     });
 });
+
